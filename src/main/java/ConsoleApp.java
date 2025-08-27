d917a14b5d546969cac5b4d9a992ae7a9772f19c
trl <lukas.traut@medidata.ch>
2025-08-27 07:36:04 +0200

    exit Abfrage zum schliessen

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -1,15 +1,35 @@
+
+import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
+import java.util.Scanner;
+
 @QuarkusMain
 public class ConsoleApp implements QuarkusApplication {
-    @Override
 
+    @Override
     public int run(String... args) throws Exception {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
-        }
-        return 0;
-    }}
+        System.out.println("Hello World!");
+        System.out.println("Geben Sie 'exit' ein, um das Programm zu beenden:");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // Endlosschleife bis "exit" eingegeben wird
+        do {
+            System.out.print("> ");
+            input = scanner.nextLine();
+        } while (!"exit".equals(input));
+
+        System.out.println("Programm wird beendet...");
+        scanner.close();
+
+        return 0; // Erfolgreiche Beendigung
+    }
+
+    public static void main(String[] args) {
+        Quarkus.run(ConsoleApp.class, args);
+    }
+}
+
