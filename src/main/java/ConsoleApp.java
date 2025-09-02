0ba82430ee2811c2ee3afcfcb052a5036f939bdc
trl <lukas.traut@medidata.ch>
2025-09-02 14:03:15 +0200

    Hinzufügen und Abfrage der Lernkarten

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -10,23 +10,61 @@ public class ConsoleApp implements QuarkusApplication {
 
     @Override
     public int run(String... args) throws Exception {
-        System.out.println("Hello World!");
-        System.out.println("Geben Sie 'exit' ein, um das Programm zu beenden:");
+        String antwort = "";
+        String lernkarte = "";
+        while (true) {
+            System.out.println("h = Hinzufügen/ l = Lernen/ show all = Alle anzeigen/ exit = Programm schliessen");
 
-        Scanner scanner = new Scanner(System.in);
-        String input;
+            Scanner scanner = new Scanner(System.in);
+            String input;
 
-        // Endlosschleife bis "exit" eingegeben wird
-        do {
+
             System.out.print("> ");
             input = scanner.nextLine();
-        } while (!"exit".equals(input));
 
-        System.out.println("Programm wird beendet...");
-        scanner.close();
+            if ("exit".equals(input)) {
 
-        return 0; // Erfolgreiche Beendigung
+                System.out.println("Programm wird beendet...");
+                scanner.close();
+                return 0;
+            }
+
+
+            if ("h".equals(input)) {
+
+                System.out.println("Gib eine Frage ein");
+                System.out.print("> ");
+                Scanner newlernkarte = new Scanner(System.in);
+                 lernkarte = newlernkarte.nextLine();
+
+                System.out.println("Gib die Antwort ein");
+                System.out.print("> ");
+                Scanner newantwort = new Scanner(System.in);
+                 antwort = newantwort.nextLine();
+
+                System.out.println("Lernkarte wurde mit der Frage " + lernkarte + " hinzugefügt");
+            }
+
+            if ("l".equals(input)) {
+                System.out.println("Gib die Antwort von der Frage " + lernkarte + " ein");
+                System.out.print("> ");
+                Scanner newlernantwort = new Scanner(System.in);
+                 lernkarte = newlernantwort.nextLine();
+                    if (!lernkarte.equals(antwort)) {
+                System.out.println("Die Antwort ist" + antwort);}
+                    else {
+                        System.out.println("Die Antwort ist richtig");
+                    }
+            }
+
+
+            if ("show all".equals(input)) {
+
+                System.out.println("Deine vorhandenen Karten...");
+            }
+        }
     }
+
 
     public static void main(String[] args) {
         Quarkus.run(ConsoleApp.class, args);
