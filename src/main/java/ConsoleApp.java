af51f989bb495920929194f79d62e2006ddb4ad3
trl <lukas.traut@medidata.ch>
2025-10-07 11:32:49 +0200

    English names

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -1,4 +1,3 @@
-
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
@@ -13,19 +12,19 @@ import java.time.format.DateTimeFormatter;
 @QuarkusMain
 public class ConsoleApp implements QuarkusApplication {
 
-    static int nächsteID = 1;
+    static int nextID = 0;
 
-    public class Karte{
+    public class Card{
         int ID;
-        String frage;
-        String antwort;
-        LocalDate erstellungsdatum;
+        String question;
+        String answer;
+        LocalDate builddate;
 
-        public Karte(String frage, String antwort, String datumString) {
-            this.ID = nächsteID++;
-            this.frage = frage;
-            this.antwort = antwort;
-            this.erstellungsdatum = LocalDate.parse(datumString);
+        public Card(String question, String answer, String dateString) {
+            this.ID = nextID++;
+            this.question = question;
+            this.answer = answer;
+            this.builddate = LocalDate.parse(dateString);
         }
 
 
@@ -36,15 +35,15 @@ public class ConsoleApp implements QuarkusApplication {
         Scanner scanner = new Scanner(System.in);
 
 
-        List<Karte> karten = new ArrayList<>();
-        karten.add(new Karte("Wann wurde Frankfurt gegründet?", "1899", "2025-09-04"));
-        karten.add(new Karte("Wie heisst die schönste Stadt auf der Welt?", "Frankfurt", "2025-09-05"));
-        karten.add(new Karte("UF", "97", "2025-04-05"));
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card("Wann wurde Frankfurt gegründet?", "1899", "2025-09-04"));
+        cards.add(new Card("Wie heisst die schönste Stadt auf der Welt?", "Frankfurt", "2025-09-05"));
+        cards.add(new Card("UF", "97", "2025-04-05"));
 
-         int i= 0;
+
 
         while (true) {
-            System.out.println("h = Hinzufügen/ l = Lernen/ show all = Alle anzeigen/ exit = Programm schliessen");
+            System.out.println("show all = Alle anzeigen/ exit = Programm schliessen");
 
             String input;
 
@@ -60,49 +59,21 @@ public class ConsoleApp implements QuarkusApplication {
             }
 
 
-            if ("h".equals(input)) {
 
-                System.out.println("Diese Funktion ist noch nicht möglich!!!");
-
-
-            }
             if ("show all".equals(input)){
 
                 System.out.println("Deine vorhandenen Karten...");
-                karten.sort(Comparator.comparing(karte -> karte.erstellungsdatum));
+                cards.sort(Comparator.comparing(card -> card.builddate));
 
-                for (int ks = 0; ks < karten.size(); ks++) {
-                    Karte aktuelleKarte = karten.get(ks);
-                    if (!aktuelleKarte.frage.equals(" ")) {
-                        System.out.println(aktuelleKarte.frage + " " + aktuelleKarte.erstellungsdatum);
+                for (int ks = 0; ks < cards.size(); ks++) {
+                    Card aktuelleKarte = cards.get(ks);
+                    if (!aktuelleKarte.question.equals(" ")) {
+                        System.out.println(aktuelleKarte.ID + " " + aktuelleKarte.question + " " + aktuelleKarte.builddate);
                     }
                 }
-            }
-
-            if ("l".equals(input)) {
-
-                if (i < karten.size()) {
-                    Karte aktuelleKarte = karten.get(i);
-                    System.out.println("Gib die Antwort auf die Frage: " + aktuelleKarte.frage);
-                    System.out.print("> ");
-                    String antwort = scanner.nextLine();
-
-                    if (!antwort.equals(aktuelleKarte.antwort)) {
-                        System.out.println("Die Antwort ist: " + aktuelleKarte.antwort);
-                    } else {
-                        System.out.println("Die Antwort ist richtig!");
-                        i++;
-                    }
-
-                }
-
-
-
-            }
-        }}
+            }}}
 
     public static void main(String[] args) {
         Quarkus.run(ConsoleApp.class, args);
     }
 }
-
