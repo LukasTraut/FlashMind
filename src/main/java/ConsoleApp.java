5911c64ec0d60f730b50f252efc68e137d1a6f6e
trl <lukas.traut@medidata.ch>
2025-11-25 11:14:57 +0100

    richtig formatiert

--- src/main/java/ConsoleApp.java
+++ src/main/java/ConsoleApp.java
@@ -9,15 +9,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-
-
-
 @QuarkusMain
 public class ConsoleApp implements QuarkusApplication {
 
     static int nextId = 0;
 
-    public class Card{
+    public class Card {
         int id;
         String question;
         String answer;
@@ -42,7 +39,6 @@ public class ConsoleApp implements QuarkusApplication {
         cards.add(new Card("Wann wurde Frankfurt gegründet?", "1899", "2025-09-04"));
         cards.add(new Card("Wie heisst die schönste Stadt auf der Welt?", "Frankfurt", "2025-09-05"));
         cards.add(new Card("Wann wurde Eintracht Frankfurt Deutscher Meister?", "1959", "2025-04-05"));
-
 
 
         while (true) {
@@ -75,33 +71,32 @@ public class ConsoleApp implements QuarkusApplication {
                         break;
                     }
                 }
-                int maxId = cards.size()-1;
-                 if(idNumber > maxId) {
+                int maxId = cards.size() - 1;
+                if (idNumber > maxId) {
                     System.out.printf("\u001B[31mEs sind nur %d Karten vorhanden.%n\u001B[0m", cards.size());
                     continue;
 
                 }
 
-                if ( !currentCard.question.trim().equals("")) {
+                if (!currentCard.question.trim().equals("")) {
                     System.out.println("Frage: " + currentCard.question);
                     System.out.print("Antwort: ");
                     Scanner newLearnAnswer = new Scanner(System.in);
                     String answer1 = newLearnAnswer.nextLine();
                     if (answer1.length() <= 251) {
                         if (!answer1.equals(currentCard.answer)) {
-                            System.out.println("\u001B[31mDie Antwort ist: \u001B[0m" + currentCard.answer );
+                            System.out.println("\u001B[31mDie Antwort ist: \u001B[0m" + currentCard.answer);
                         } else {
                             System.out.println("\u001B[32mDie Antwort ist richtig\u001B[0m");
                         }
-                    }
-                    else{
+                    } else {
                         System.out.println("\u001B[31mZu lang, maximal 250 Zeichen!!!!\u001B[0m");
                     }
                 }
 
             }
 
-            if ("show all".equals(input) || "Show all".equals(input)|| "Show All".equals(input)){
+            if ("show all".equals(input) || "Show all".equals(input) || "Show All".equals(input)) {
 
                 System.out.println("Deine vorhandenen Karten...");
                 cards.sort(Comparator.comparing(card -> card.buildDate));
@@ -120,7 +115,7 @@ public class ConsoleApp implements QuarkusApplication {
                 }
             }
 
-            if( !"exit".equals(input) && !"Exit".equals(input) && !"show all".equals(input) && !"Show all".equals(input) && !"learn".equals(input) && !"Learn".equals(input)) {
+            if (!"exit".equals(input) && !"Exit".equals(input) && !"show all".equals(input) && !"Show all".equals(input) && !"learn".equals(input) && !"Learn".equals(input)) {
 
                 System.out.println("\u001B[31mBefehl nicht erkannt! Bitte überprüfe die Schreibweise und versuche es erneut.\u001B[0m");
             }
